38dcfb0b45a2e52ee47176624e5012992620b78d
keychainbalee <muhammad.iqbal.sap@gmail.com>
2026-07-06 21:15:31 +0700

    feat : fitur look camera for android

--- Assets/Scripts/UI/LookController.cs
+++ Assets/Scripts/UI/LookController.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class LookController : MonoBehaviour
+public class LookController : MonoBehaviour,
+    IPointerDownHandler,
+    IDragHandler,
+    IPointerUpHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private float sensitivity = 0.2f;
+
+    private Vector2 lastPosition;
+    private bool isDragging;
+
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        lastPosition = eventData.position;
+        isDragging = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        if (!isDragging) return;
+
+        Vector2 delta = eventData.position - lastPosition;
+
+        playerMovement.SetLookInput(delta * sensitivity);
+
+        lastPosition = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+        playerMovement.SetLookInput(Vector2.zero);
     }
 }
